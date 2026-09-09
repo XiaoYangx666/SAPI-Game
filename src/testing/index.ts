633a252824e4ffa6a1088033a65ff8bb35fdd32b
XiaoYangx666 <111029916+XiaoYangx666@users.noreply.github.com>
2026-09-09 14:08:05 +0800

    refactor: 导出 BEGameTestEngine

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -1,4 +1,4 @@
-export { SAPIGameTestEngine } from "./testEngine";
+export { BEGameTestEngine, SAPIGameTestEngine } from "./testEngine";
 export type { ReloadScenario, TestTraceEntry } from "./testEngine";
 export { virtualMinecraft, Player as TestPlayer } from "./virtualMinecraft";
 export { virtualMinecraftUi } from "./virtualMinecraftUi";
