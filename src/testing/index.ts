331f73eb1cd25affe35f3c27cfe595bb2e13ad6c
XiaoYangx666 <111029916+XiaoYangx666@users.noreply.github.com>
2026-09-10 12:09:18 +0800

    feat(test): export trace sinks

--- src/testing/index.ts
+++ src/testing/index.ts
@@ -1,4 +1,6 @@
 export { BEGameTestEngine, SAPIGameTestEngine } from "./testEngine";
 export type { ReloadScenario, TestTraceEntry } from "./testEngine";
+export { FileTraceSink, TestTraceSink } from "./traceSink";
+export type { TestTraceRecord } from "./traceSink";
 export { virtualMinecraft, Player as TestPlayer } from "./scriptApiShell";
 export { virtualMinecraftUi } from "./virtualMinecraftUi";
